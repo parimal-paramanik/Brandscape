9c5f3e1a974a38f8b05669d837a3063209af16ce
error-4u <kumarvishal1082002@gmail.com>
2024-04-06 14:18:01 +0530

    image ai tool

--- src/Components/AiTool.jsx
+++ src/Components/AiTool.jsx
@@ -14,16 +14,16 @@ function AiTool() {
     </div>
   </div>
 </div>
-<center>
-<div className="container relative border align-center items-center pb-8 mt-20">
+{/* <center> */}
+<div className="container align-center items-center pb-8 mt-20 max-w-9xl mx-auto">
 
 <div className="grid grid-cols-1 gap-4 rounded-md p-4 lg:grid-cols-2">
   <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
 <h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
-<div className="block"></div>
+<div className="block">
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
     <a href=""> Personal assistant </a>
@@ -53,16 +53,21 @@ function AiTool() {
     <a href=""> Presentations </a>
     <span>(48)</span>
   </div>
+</div>
+
+</div>
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Productivity Tools</a>
 </div>
 
 </div>
 </div>
 
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">AI Video tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -94,14 +99,58 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Video Tools</a>
+</div>
+</div>
 
+</div>
+<div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
+
+<div className="flex flex-col">
+<h2 className="text-3xl font-semibold text-slate-800">AI Text Generators</h2>
+<div className="block"></div>
+<div className="capitalize">
+  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
+    <a href=""> Personal assistant </a>
+    <span>(300)</span>
+  </div>
+</div>
+<div className="capitalize">
+  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
+    <a href=""> Research </a>
+    <span>(291)</span>
+  </div>
+</div>
+<div className="capitalize">
+  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
+    <a href=""> Spreadsheets </a>
+    <span>(47)</span>
+  </div>
+</div>
+<div className="capitalize">
+  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
+    <a href=""> Translator </a>
+    <span>(69)</span>
+  </div>
+</div>
+<div className="capitalize">
+  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
+    <a href=""> Presentations </a>
+    <span>(48)</span>
+  </div>
+</div>
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Text Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">AI Business Tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -133,14 +182,16 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Buisness Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">AI Image Tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -172,14 +223,16 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Image Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">Automation Tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -211,14 +264,16 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Automation Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">AI Art Generators</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -250,14 +305,16 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Art Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">Misc AI Tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -289,14 +346,16 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Misc Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">AI Code Tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -328,14 +387,16 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Code Tools</a>
+</div>
 </div>
 </div>
 <div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
+   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] " />
 
 <div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
+<h2 className="text-3xl font-semibold text-slate-800">AI Audio tools</h2>
 <div className="block"></div>
 <div className="capitalize">
   <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
@@ -367,46 +428,9 @@ function AiTool() {
     <span>(48)</span>
   </div>
 </div>
-
+<div className='mt-auto pt-4'>
+  <a href="" className='text-blue-500 transition-colors hover:text-blue-400 text-start' >Show all AI Audio Tools</a>
 </div>
-</div>
-<div className="flex flex-row gap-2 rounded-lg bg-white p-4 shadow-xl">
-   <img src="src/Assets/aitool.webp" alt="" loading="lazy" width="400" height="400" className="aspect-square w-full rounded-lg md:h-[200px] md:w-[200px] border" />
-
-<div className="flex flex-col">
-<h2 className="text-3xl font-semibold text-slate-800">AI productivity tools</h2>
-<div className="block"></div>
-<div className="capitalize">
-  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
-    <a href=""> Personal assistant </a>
-    <span>(300)</span>
-  </div>
-</div>
-<div className="capitalize">
-  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
-    <a href=""> Research </a>
-    <span>(291)</span>
-  </div>
-</div>
-<div className="capitalize">
-  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
-    <a href=""> Spreadsheets </a>
-    <span>(47)</span>
-  </div>
-</div>
-<div className="capitalize">
-  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
-    <a href=""> Translator </a>
-    <span>(69)</span>
-  </div>
-</div>
-<div className="capitalize">
-  <div className="flex gap-1 text-slate-800 transition-colors hover:text-slate-500">
-    <a href=""> Presentations </a>
-    <span>(48)</span>
-  </div>
-</div>
-
 </div>
 </div>
 
@@ -415,7 +439,7 @@ function AiTool() {
 
 </div>
 
-</center>
+{/* </center> */}
 
 </div>
     </div>
